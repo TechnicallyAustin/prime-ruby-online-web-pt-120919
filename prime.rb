0faf88371ef7c30a5bd04532d2a6a3a58dec27b0
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 04:11:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,10 @@ def prime?(num)
 i = 0
 prime = num % 2 
 range = (-2..(num - 1)).to_a
-while range.length > i do
-  new_num = num /range[i]
-  if new_num
-binding.pry
+
+if prime == 0 
+  TRUE
+else
+  FALSE
 end
-  
+end 
