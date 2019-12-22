6d5801d627cb41897857c76e7fa212595a3e6ce3
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 03:56:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,7 @@
 def prime?(num)
 i = 2
-new_arr = (-1000...1000).to_a
+new_arr = (-num...num).to_a
 prime = num % 2 
-while i < num do
-  if prime == 0
-    FALSE
-    i += 1 
-  end
-  true
-end
-end
+
+new_arr.collect do |prime_arr|
+  
