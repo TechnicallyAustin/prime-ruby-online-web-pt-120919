0538db4fad22a05ab1a35c0bd6f96ca6e07a24a1
Austin Johnson <technicallyaustin@gmail.com>
2019-12-22 03:45:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 def prime?(num)
 by_two = num % 2 
 div_two = num / 2
-square = 
+square = num / 
 if by_two == 0 && num != 2
   FALSE
 elsif by_two != 0 
-while div_two 
+while div_two < 
   
   
   
